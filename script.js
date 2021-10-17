698c0b9c37c8d152aadb5a6af07957955081255b
Lavish Jain <lavishjain36@gmail.com>
2021-10-17 17:30:03 +0530

    bs

--- script.js
+++
@@ -1,9 +0,0 @@
-// From the below repo link of files, you can pick any two and complete them with a responsive design.
-
-// For your reference visit this URL
-// https://github.com/rvsp/HTML-CSS/tree/master/Tasks
-
-// github repo=>
-// css repo=>
-// 1.deployable
-// 2.
